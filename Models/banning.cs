e1dd8174283649eea641676ad03fd8df556a18b7
kidfearless <kidfearless1@gmail.com>
2020-11-30 19:11:59 -0700

    Mass changes to files that can be done with find and replace

---
+++ Models/banning.cs
@@ -0,0 +1,134 @@
+/**
+* vim: set ts=4 :
+* =============================================================================
+* SourceMod (C)2004-2008 AlliedModders LLC.  All rights reserved.
+* =============================================================================
+*
+* This file is part of the SourceMod/SourcePawn SDK.
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, version 3.0, as published by the
+* Free Software Foundation.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* As a special exception, AlliedModders LLC gives you permission to link the
+* code of this program (as well as its derivative works) to "Half-Life 2," the
+* "Source Engine," the "SourcePawn JIT," and any Game MODs that run on software
+* by the Valve Corporation.  You must obey the GNU General Public License in
+* all respects for all other code used.  Additionally, AlliedModders LLC grants
+* this exception to all derivative works.  AlliedModders LLC defines further
+* exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
+* or <http://www.sourcemod.net/license.php>.
+*
+* Version: $Id$
+*/
+namespace Sourcemod
+{
+	public partial class SourceMod
+	{
+
+		public const int BANFLAG_AUTO = (1 << 0);  /**< Auto-detects whether to ban by steamid or IP */
+		public const int BANFLAG_IP =         (1 << 1);  /**< Always ban by IP address */
+		public const int BANFLAG_AUTHID  =    (1 << 2);  /**< Always ban by authstring (for BanIdentity) if possible */
+		public const int BANFLAG_NOKICK  =    (1 << 3);  /**< Does not kick the client */
+
+		/**
+		 * Called for calls to BanClient() with a non-empty command.
+		 *
+		 * @param client        Client being banned.
+		 * @param time          Time the client is being banned for (0 = permanent).
+		 * @param flags         One if AUTHID or IP will be enabled.  If AUTO is also 
+		 *                      enabled, it means Core autodetected which to use.
+		 * @param reason        Reason passed via BanClient().
+		 * @param kick_message  Kick message passed via BanClient().
+		 * @param command       Command string to identify the ban source.
+		 * @param source        Source value passed via BanClient().
+		 * @return              Plugin_Handled to block the actual server banning.
+		 *                      Kicking will still occur.
+		 */
+public static override Action OnBanClient(int client, int time, int flags, string reason, string kick_message, string command, any source) { throw new NotImplementedException(); }
+
+	/**
+	 * Called for calls to BanIdentity() with a non-empty command.
+	 *
+	 * @param identity      Identity string being banned (authstring or ip).
+	 * @param time          Time the client is being banned for (0 = permanent).
+	 * @param flags         Ban flags (only IP or AUTHID are valid here).
+	 * @param reason        Reason passed via BanIdentity().
+	 * @param command       Command string to identify the ban source.
+	 * @param source        Source value passed via BanIdentity().
+	 * @return              Plugin_Handled to block the actual server banning.
+	 */
+	public static override Action OnBanIdentity(string identity, int time, int flags, string reason, string command, any source)
+	{ throw new NotImplementedException(); }
+
+	/**
+	 * Called for calls to RemoveBan() with a non-empty command.
+	 *
+	 * @param identity      Identity string being banned (authstring or ip).
+	 * @param flags         Ban flags (only IP or AUTHID are valid here).
+	 * @param command       Command string to identify the ban source.
+	 * @param source        Source value passed via BanIdentity().
+	 * @return              Plugin_Handled to block the actual unbanning.
+	 */
+	public static override Action OnRemoveBan(string identity, int flags, string command, any source)
+	{ throw new NotImplementedException(); }
+
+	/**
+	 * Bans a client.
+	 *
+	 * @param client        Client being banned.
+	 * @param time          Time (in minutes) to ban (0 = permanent).
+	 * @param flags         Flags for controlling the ban mechanism.  If AUTHID 
+	 *                      is set and no AUTHID is available, the ban will fail 
+	 *                      unless AUTO is also flagged.
+	 * @param reason        Reason to ban the client for.
+	 * @param kick_message  Message to display to the user when kicking.
+	 * @param command       Command string to identify the source.  If this is left 
+	 *                      empty, then the OnBanClient public static override will not be called.
+	 * @param source        A source value that could be interpreted as a player 
+	 *                      index of any sort (not actually checked by Core).
+	 * @return              True on success, false on failure.
+	 * @error               Invalid client index or client not in game.
+	 */
+	public static bool BanClient(int client, int time, int flags, string reason, string kick_message = "", string command = "", any source = 0)
+	{ throw new NotImplementedException(); }
+
+	/**
+	 * Bans an identity (either an IP address or auth string).
+	 *
+	 * @param identity      String to ban (ip or authstring).
+	 * @param time          Time to ban for (0 = permanent).
+	 * @param flags         Flags (only IP and AUTHID are valid flags here).
+	 * @param reason        Ban reason string.
+	 * @param command       Command string to identify the source.  If this is left 
+	 *                      empty, then the OnBanIdentity public static override will not be called.
+	 * @param source        A source value that could be interpreted as a player
+	 *                      index of any sort (not actually checked by Core).
+	 * @return              True on success, false on failure.
+	 */
+	public static bool BanIdentity(string identity, int time, int flags, string reason, string command = "", any source = 0)
+	{ throw new NotImplementedException(); }
+
+	/**
+	 * Removes a ban that was written to the server (either in memory or on disk).
+	 *
+	 * @param identity      String to unban (ip or authstring).
+	 * @param flags         Flags (only IP and AUTHID are valid flags here).
+	 * @param command       Command string to identify the source.  If this is left 
+	 *                      empty, then OnRemoveBan will not be called.
+	 * @param source        A source value that could be interpreted as a player 
+	 *                      index of any sort (not actually checked by Core).
+	 * @return              True on success, false on failure.
+	 */
+	public static bool RemoveBan(string identity, int flags, string command = "", any source = 0)
+	{ throw new NotImplementedException(); }
+}
+}
---
+++ Models/core.cs
@@ -0,0 +1,185 @@
+/**
+* vim: set ts=4 sw=4 tw=99 noet:
+* =============================================================================
+* SourceMod (C)2004-2008 AlliedModders LLC.  All rights reserved.
+* =============================================================================
+*
+* This file is part of the SourceMod/SourcePawn SDK.
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, version 3.0, as published by the
+* Free Software Foundation.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* As a special exception, AlliedModders LLC gives you permission to link the
+* code of this program (as well as its derivative works) to "Half-Life 2," the
+* "Source Engine," the "SourcePawn JIT," and any Game MODs that run on software
+* by the Valve Corporation.  You must obey the GNU General Public License in
+* all respects for all other code used.  Additionally, AlliedModders LLC grants
+* this exception to all derivative works.  AlliedModders LLC defines further
+* exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
+* or <http://www.sourcemod.net/license.php>.
+*
+* Version: $Id$
+*/
+
+using System;
+using System.Collections.ObjectModel;
+namespace Sourcemod
+{
+	public partial class SourceMod
+	{
+
+		/** If this gets changed, you need to update Core's check. */
+		public const int SOURCEMOD_PLUGINAPI_VERSION = 5;
+
+		public struct PlVers
+		{
+			public int version;
+			public string filevers;
+			public string date;
+			public string time;
+		};
+
+		/**
+		 * Specifies what to do after a hook completes.
+		 */
+		public enum Action
+		{
+			Plugin_Continue = 0,    /**< Continue with the original action */
+			Plugin_Changed = 1,     /**< Inputs or outputs have been overridden with new values */
+			Plugin_Handled = 3,     /**< Handle the action at the end (don't call it) */
+			Plugin_Stop = 4         /**< Immediately stop the hook chain and handle the original */
+		};
+
+		/**
+		 * Specifies identity types.
+		 */
+		public enum Identity
+		{
+			Identity_Core = 0,
+			Identity_Extension = 1,
+			Identity_Plugin = 2
+		};
+
+		public PlVers __version = new PlVers()
+		{
+			version = SOURCEMOD_PLUGINAPI_VERSION,
+			filevers = SOURCEMOD_VERSION,
+			date = DateTime.Now.Date.ToString(),
+			time = DateTime.Now.TimeOfDay.ToString()
+		};
+
+		/**
+		 * Plugin status values.
+		 */
+		public enum PluginStatus
+		{
+			Plugin_Running = 0,       /**< Plugin is running */
+			/* All states below are "temporarily" unexecutable */
+			Plugin_Paused,          /**< Plugin is loaded but paused */
+			Plugin_Error,           /**< Plugin is loaded but errored/locked */
+			/* All states below do not have all natives */
+			Plugin_Loaded,          /**< Plugin has passed loading and can be finalized */
+			Plugin_Failed,          /**< Plugin has a fatal failure */
+			Plugin_Created,         /**< Plugin is created but not initialized */
+			Plugin_Uncompiled,      /**< Plugin is not yet compiled by the JIT */
+			Plugin_BadLoad,         /**< Plugin failed to load */
+			Plugin_Evicted          /**< Plugin was unloaded due to an error */
+		};
+
+		/**
+		 * Plugin information properties. Plugins can declare a global variable with
+		 * their info. Example,
+		 *
+		 *   public Plugin myinfo = {
+		 *   	name = "Admin Help",
+		 *   	author = "AlliedModders LLC",
+		 *   	description = "Display command information",
+		 *   	version = "1.0",
+		 *   	url = "http://www.sourcemod.net/"
+		 *   };
+		 *
+		 * SourceMod will display this information when a user inspects plugins in the
+		 * console.
+		 */
+		public enum PluginInfo
+		{
+			PlInfo_Name,            /**< Plugin name */
+			PlInfo_Author,          /**< Plugin author */
+			PlInfo_Description,     /**< Plugin description */
+			PlInfo_Version,         /**< Plugin version */
+			PlInfo_URL              /**< Plugin URL */
+		};
+
+		/**
+		 * Defines how an extension must expose itself for autoloading.
+		 */
+		public struct Extension
+		{
+			public string name;   /**< Short name */
+			public string file;   /**< Default file name */
+			public bool autoload;       /**< Whether or not to auto-load */
+			public bool required;       /**< Whether or not to require */
+		};
+
+		/**
+		 * Defines how a plugin must expose itself for native requiring.
+		 */
+		public struct SharedPlugin
+		{
+			public string name;   /**< Short name */
+			public string file;   /**< File name */
+			public bool required;       /**< Whether or not to require */
+		};
+
+		public static readonly float[] NULL_VECTOR = new float[3];        /**< Pass this into certain functions to act as a C++ NULL */
+		public static readonly string NULL_STRING = string.Empty;   /**< pass this into certain functions to act as a C++ NULL */
+
+		/**
+		 * Check if the given vector is the NULL_VECTOR.
+		 *
+		 * @param vec     The vector to test.
+		 * @return        True if NULL_VECTOR, false otherwise.
+		 */
+		public static bool IsNullVector(ReadOnlyCollection<float> vec) { throw new NotImplementedException(); }
+
+		/**
+		 * Check if the given string is the NULL_STRING.
+		 *
+		 * @param str     The string to test.
+		 * @return        True if NULL_STRING, false otherwise.
+		 */
+		public static bool IsNullString(string str) { throw new NotImplementedException(); }
+
+		/**
+		 * Horrible compatibility shim.
+		 */
+		public Extension __ext_core = new()
+		{
+			name = "Core",
+			file = "core",
+			autoload = false,
+			required = false,
+		};
+
+		public static int VerifyCoreVersion() { throw new NotImplementedException(); }
+
+		/**
+		 * Sets a native as optional, such that if it is unloaded, removed,
+		 * or otherwise non-existent, the plugin will still work.  Calling
+		 * removed natives results in a run-time error.
+		 *
+		 * @param name          Native name.
+		 */
+		public static void MarkNativeAsOptional(string name) { throw new NotImplementedException(); }
+
+	}
+}
